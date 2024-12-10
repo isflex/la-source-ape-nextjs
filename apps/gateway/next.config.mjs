ac24944d78b08c925a10d2406bd7880cb37bbba3
ischerer <hello@flexiness.com>
2024-12-10 10:41:32 +0100

    init monorepo

---
+++ apps/gateway/next.config.mjs
@@ -0,0 +1,11 @@
+const mainConfig = (() => {
+
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    reactStrictMode: true,
+  }
+
+  return nextConfig
+})
+
+export default mainConfig
