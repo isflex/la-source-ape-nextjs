2150fbf55c62830a71b9828930a2bfc9405410c9
ischerer <hello@flexiness.com>
2024-12-13 09:51:32 +0100

    rework monorepo pnpm workspaces

--- bin/setPkgManagerConfig.sh
+++ bin/setPkgManagerConfig.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# npm install --package-lock-only --dry-run;
-# pnpm import && rm package-lock.json && pnpm i --frozen-lockfile --shamefully-hoist;
-
 if [[ ! -z ${CI} ]]
 then
   echo 'CI mode';
   cd $FLEX_APP/bin;
   pnpm config set prefer-frozen-lockfile true;
   pnpm config set auto-install-peers false;
+  pnpm config set link-workspace-packages true;
   pnpm config set node-linker 'hoisted';
   pnpm config set enable-pre-post-scripts true;
   pnpm config set store-dir '.pnpm-store';
@@ -17,6 +15,7 @@ else
   cd $FLEX_PROJ_ROOT/bin;
   pnpm config set prefer-frozen-lockfile false;
   pnpm config set auto-install-peers true;
+  pnpm config set link-workspace-packages true;
   # pnpm config set use-node-version 20.15.0;
 fi
 
@@ -24,6 +23,7 @@ source ~/.profile;
 
 echo "prefer-frozen-lockfile : $(pnpm config get prefer-frozen-lockfile)";
 echo "auto-install-peers : $(pnpm config get auto-install-peers)";
+echo "link-workspace-packages : $(pnpm config get link-workspace-packages)";
 echo "node-linker : $(pnpm config get node-linker)";
 echo "enable-pre-post-scripts : $(pnpm config get enable-pre-post-scripts)";
 echo "store-dir : $(pnpm config get store-dir)";
