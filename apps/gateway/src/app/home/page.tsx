a6b923bcea0809c4f24623172b2a32fe5c90f81f
ischerer <hello@flexiness.com>
2025-01-27 23:29:57 +0100

    update /home page

--- apps/gateway/src/app/home/page.tsx
+++ apps/gateway/src/app/home/page.tsx
@@ -7,47 +7,39 @@ import Link from 'next/link'
 import type { NextPage } from 'next'
 import { PageAppProps } from '@root/types/additional'
 
-// import classNames from 'classnames'
-// import {
-//   // flexStyles,
-//   // Button,
-//   // ButtonMarkup,
-//   Box,
-//   // Link,
-//   Text,
-//   Title,
-//   TitleLevel,
-//   // VariantState,
-//   IconName,
-//   InfoBlock,
-//   InfoBlockAction,
-//   InfoBlockContent,
-//   InfoBlockHeader,
-//   InfoBlockStatus,
-// } from '@flex-design-system/react-ts/client-sync-styled-default'
-// import { default as flexStyles } from '@src/styles/scss/flex/all.module.scss'
+import classNames from 'classnames'
+import {
+  // flexStyles,
+  // Button,
+  // ButtonMarkup,
+  Box,
+  // Link,
+  Text,
+  Title,
+  TitleLevel,
+  // VariantState,
+  IconName,
+  InfoBlock,
+  InfoBlockAction,
+  InfoBlockContent,
+  InfoBlockHeader,
+  InfoBlockStatus,
+} from '@flex-design-system/react-ts/client-sync-styled-default'
+import { default as flexStyles } from '@src/styles/scss/flex/all.module.scss'
 
 const Home: NextPage<PageAppProps> = () => {
   return (
-    <>
-    {/* <Box className={classNames(flexStyles.isFlex, flexStyles.isFlexDirectionColumn, flexStyles.isAlignItemsCenter)}>
-      <Text className={flexStyles.isItalic}>Contenue de la page à venir avec l'aide des élèves de la source 🤞</Text>
-    </Box> */}
-
-    {/* <InfoBlock>
-      <InfoBlockHeader status={InfoBlockStatus.INFO} customIcon={IconName.UI_INFO_CIRCLE}>
-        <Title level={TitleLevel.LEVEL3}>Contenu de la page à venir</Title>
-      </InfoBlockHeader>
-      <InfoBlockContent>
-        <Title level={TitleLevel.LEVEL4}>avec l'aide des élèves de la source 🤞</Title>
-      </InfoBlockContent>
-      <InfoBlockAction>
-        <Link href='/about' className={flexStyles.link}>Qui somme nous</Link>
-      </InfoBlockAction>
-    </InfoBlock> */}
-
-    <div>Something</div>
-    </>
+  <InfoBlock>
+    <InfoBlockHeader status={InfoBlockStatus.INFO} customIcon={IconName.UI_INFO_CIRCLE}>
+      <Title level={TitleLevel.LEVEL3}>Contenu de la page à venir</Title>
+    </InfoBlockHeader>
+    <InfoBlockContent>
+      <Title level={TitleLevel.LEVEL4}>avec l'aide des élèves de la source 🤞</Title>
+    </InfoBlockContent>
+    <InfoBlockAction>
+      <Link href='/about' className={flexStyles.link}>Qui somme nous</Link>
+    </InfoBlockAction>
+  </InfoBlock>
   )
 }
 
