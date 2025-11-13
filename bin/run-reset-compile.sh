c31509e796dcb2689d58b65e44ecaebcca58ba1b
ischerer <hello@flexiness.com>
2025-11-13 23:05:00 +0100

    only data

---
+++ bin/run-reset-compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd $FLEX_PROJ_ROOT
+pnpm exec rimraf -g '**/.turbo' -g '**/.webpack-cache' -g '**/.eslintcache'
+# pnpm exec rimraf -g '**/prebuild'
+# pnpm exec rimraf -g '**/bundle'
+pnpm exec rimraf -g '**/dist' '!packages/flex/config/typed-scss-modules/dist'
+# pnpm exec rimraf -g 'apps/**/dist'
+pnpm exec rimraf -g '**/*.tsbuildinfo'
+# pnpm exec rimraf -g '**/build' -g '**/server-build'
+# pnpm exec rimraf -g '**/node_modules'
